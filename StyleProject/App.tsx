f821c5114c03855fe23c020b3381289a206fc88f
Sujay Kanakamedala <98435886+Dragneel005@users.noreply.github.com>
2024-06-07 12:23:19 -0700

    Add files via upload

---
+++ StyleProject/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Balance from './components/Balance'
+
+export default function App() {
+  return (
+    <View>
+      <Balance/>
+    </View>
+  )
+}
+
